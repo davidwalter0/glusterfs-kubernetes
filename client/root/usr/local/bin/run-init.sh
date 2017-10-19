59289a02bce92bb165a095a7af54c6481d9d453f
David Walter <walter.david@gmail.com>
2017-10-19 01:11:04 -0400

    systemd initiating

--- client/root/usr/local/bin/run-init.sh
+++ client/root/usr/local/bin/run-init.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/dumb-init /bin/sh
+systemctl stop glusterd
+systemctl stop glustereventsd
+systemctl stop rpcbind
+systemctl disable glusterd
+systemctl disable glustereventsd
+systemctl disable rpcbind
+
 mkdir -p ${GLUSTER_MOUNTPOINT}
 mount -t glusterfs ${GLUSTER_VOLUME_SERVER}:/${GLUSTER_VOLUMEID} ${GLUSTER_MOUNTPOINT}
 sleep 5
+
+MOUNTS=`mount | grep glusterfs`
+
+exec /lib/systemd/systemd
 
 MOUNTS=`mount | grep glusterfs`
 if [ -z "${MOUNTS}" ]; then
